cdd011683c3e23a25b678505252d3752fea59bce
Hunc Tasci <hunc.tasci@gmail.com>
2023-08-24 19:44:42 +0300

    create overview section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,6 +5,7 @@ import { motion } from "framer-motion";
 import { styles } from "../styles";
 import { services } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
+import { SectionWrapper } from "../hoc";
 
 const ServiceCard = ({ index, title, icon }) => {
   return (
@@ -39,11 +40,21 @@ const About = () => {
         variants={fadeIn("", "", 0.1, 1)}
         className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
       >
-        I'm a skilled software developer with experience in TypeScript and
-        JavaScript, and expertise in frameworks like React, Node.js, and
-        Three.js. I'm a quick learner and collaborate closely with clients to
-        create efficient, scalable, and user-friendly solutions that solve
-        real-world problems. Let's work together to bring your ideas to life!
+        Hey there, I'm Hunc, a frontend developer driven by a passion for
+        technology and design. With a background in marketing and media, I've
+        discovered my true calling in shaping digital experiences. My journey
+        accelerated through the Recoded Front End Development Bootcamp, where I
+        delved deep into HTML, CSS, JavaScript, React, and Next.js. A standout
+        moment was the Capstone Project—an immersive collaboration to build a
+        crowdfunding app. This experience merged technical prowess with
+        real-world agility. Drawing from my background managing online apparel
+        business websites and social media, I've honed an intuition for user
+        interfaces and engagement. Each project, be it solo or collaborative,
+        fuels my growth and curiosity. As a proud member of the exclusive
+        Recoded community, I've earned a Front End Web Developer certificate
+        from Flatiron School, New York. A communicator at heart, I value
+        collaboration and seamless teamwork. Let's together shape the digital
+        world.
       </motion.p>
 
       <div className="mt-20 flex flex-wrap gap-10">
@@ -55,4 +66,4 @@ const About = () => {
   );
 };
 
-export default About;
+export default SectionWrapper(About, "about");
